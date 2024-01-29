b0d9ee298b784094053b94fcd43581a6e1a0cb53
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-28 23:57:30 -0800

    Add github/linkedin badges to about component

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import githubLogo from "../../../public/github.svg";
+import linkedInLogo from "../../../public/linkedin.svg";
+
 const About = () => {
   return (
     <section>
       <div>
         <p>Hello! I&apos;m Terrence!</p>
         <p>I&apos;m a full-stack developer based in Vancouver, Canada.</p>
+        <section className={"flex flex-row space-x-1"}>
+          <a href={"https://github.com/ubctliu"}>
+            <Image src={githubLogo} alt={"github logo"} width={25} height={25}/>
+          </a>
+          <a href={"https://www.linkedin.com/in/terrence-liu/"}>
+            <Image src={linkedInLogo} alt={"linkedIn logo"} width={25} height={25}/>
+          </a>
+        </section>
       </div>
     </section>
   );
