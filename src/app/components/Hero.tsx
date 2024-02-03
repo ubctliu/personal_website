e50702862b0aa67d99fb3271c0ed1319c9d63d11
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-02 23:56:14 -0800

    Add intro text

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,10 @@
 const Hero = () => {
   return (
-    <></>
+    <>
+
+      <div className={"font-sans antialiased text-2xl leading-4 space-y-0"}>Hi, I&apos;m Terrence!</div>
+      <div className={"font-sans antialiased text-xl leading-4"}>I&apos;m a full-stack developer with a passion for learning new technologies and exploring new ideas!</div>
+    </>
   );
 };
 
