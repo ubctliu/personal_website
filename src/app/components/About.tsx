f117d9c61aa53bcb1b15b224aebc74492d54ece5
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-04 22:45:02 -0800

    Change text & fix anchor blocking issue on titles

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,11 +3,11 @@ import Contact from "@/app/components/Contact";
 const About = () => {
   return (
     <section className={"py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12"}>
-      <a href={"#contact"} className={"no-underline"}>
-        <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white"}>Contact me here!</div>
+      <a href={"#contact"} className={"no-underline inline-block"}>
+        <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white"}>Let&apos;s connect!</div>
       </a>
       <div className={"mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"}>
-        <p>Reach out to me if you have any opportunities for me or want to work with me on a project!</p>
+        <p>Have a project in mind? Let&apos;s chat!</p>
       </div>
       <Contact />
     </section>
