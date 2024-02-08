aacfe8e221f8dfba779c5d9fb9b41d2f13355a50
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-08 02:31:40 -0800

    Add reveal animation for hero + projects

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className={"absolute mt-4 flex flex-col w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"}>
+            <Menu.Items className={"absolute mt-4 flex flex-col w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none z-10"}>
               <Menu.Item>
                 {({ active }) => (
                   <a
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Projects from "@/app/components/Projects";
 import Hero from "@/app/components/Hero";
 import About from "@/app/components/About";
 import Header from "@/app/components/Header";
+import Reveal from "./components/utils/Reveal";
 
 export default function Home() {
   return (
@@ -9,8 +11,12 @@ export default function Home() {
       <title className="">Terrence&apos;s Portfolio</title>
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <Header />
-        <Hero />
-        <Projects />
+        <Reveal>
+          <Hero />
+        </Reveal>
+        <Reveal>
+          <Projects />
+        </Reveal>
         <About/>
       </div>
     </>
