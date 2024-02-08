006fdda3f9851aa9b69c1fe3dac4916f46d5494e
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-07 18:06:04 -0800

    Add image for odyssey & live link parameter

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,20 +1,30 @@
 import linkImage from "../../../public/link.svg";
+import liveLinkImage from "../../../public/box-arrow-up-right.svg";
+
 import Image from "next/image";
 
 type ProjectProps = {
   projectName: string,
-  projectLink: string
+  projectLink: string,
+  liveLink?: string,
+  projectImage?: string
 };
 
-const Project = ({ projectName, projectLink } : ProjectProps) => {
+const Project = ({ projectName, projectLink, liveLink, projectImage } : ProjectProps) => {
   return (
     <article>
-      <p>{projectName}</p>
-      <div>
-        <a href={projectLink} className={"inline-block"}>
-          <Image src={linkImage} alt={"project link image"} width={25} height={25}/>
-        </a>
-      </div>
+      <p>{projectName}
+        <div className={"flex justify-center space-x-2"}>
+          <a href={projectLink} className={"inline-block"}>
+            <Image src={linkImage} alt={"project link image"} width={25} height={25}/>
+          </a>
+          {liveLink &&
+        <a href={liveLink} className={"inline-block"}>
+          <Image src={liveLinkImage} alt={"live link image"} width={20} height={20}/>
+        </a>}
+        </div>
+      </p>
+      { projectImage && <Image src={projectImage} alt={"project image"}/>}
     </article>
   );
 };
