ef75655f9f36487c1d0e6bd5d307703c9c9dacd1
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-07 23:21:44 -0800

    Add string optional type to project image

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,19 +1,18 @@
 import linkImage from "../../../public/link.svg";
 import liveLinkImage from "../../../public/box-arrow-up-right.svg";
-
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 type ProjectProps = {
   projectName: string,
   projectLink: string,
   liveLink?: string,
-  projectImage?: string
+  projectImage?: string | StaticImageData;
 };
 
 const Project = ({ projectName, projectLink, liveLink, projectImage } : ProjectProps) => {
   return (
     <article>
-      {projectName}
+      <p className={"text-xl font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"}> {projectName} </p>
       <div className={"flex justify-center space-x-2"}>
         <a href={projectLink} className={"inline-block"}>
           <Image src={linkImage} alt={"project link image"} width={25} height={25}/>
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,6 @@
 import Project from "@/app/components/Project";
 import odysseyImage from "../../../public/odyssey.png";
+import photoLabsImage from "../../../public/photolabs.png";
 
 const Projects = () => {
   return (
@@ -8,7 +9,7 @@ const Projects = () => {
         <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white"}>Projects</div>
       </a>
       <Project projectName={"Odyssey"} projectLink={"https://github.com/ubctliu/odyssey"} liveLink={"https://odyssey-delta-lac.vercel.app/"} projectImage={odysseyImage} />
-      <Project projectName={"Photolabs"} projectLink={"https://github.com/ubctliu/photolabs-starter"}/>
+      <Project projectName={"Photolabs"} projectLink={"https://github.com/ubctliu/photolabs-starter"} projectImage={photoLabsImage} />
       <Project projectName={"Lighthouse Marketplace"} projectLink={"https://github.com/ubctliu/lhl-marketplace"}/>
     </section>
   );
