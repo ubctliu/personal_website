e0da1f5a6dbea72f7fb022e99748d8380ab7790d
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-09 14:35:38 -0800

    Changed technologies to languages

--- src/app/components/Personal.tsx
+++ src/app/components/Personal.tsx
@@ -4,7 +4,7 @@ const Personal = () => {
       <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 sm:text-3xl md:text-5xl lg:text-6xl dark:text-white"}>Hello, I&apos;m Terrence Liu!</div>
       <div className={"mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"}>
         I&apos;m a full-stack developer from Vancouver with a passion for learning new technologies and exploring new ideas!
-        I&apos;m primarily working with full stack technologies like TypeScript/JavaScript in a PERN stack right now, but I&apos;m versed in Java & other technologies as well - feel free to reach out if you&apos;d like to know more!
+        I&apos;m primarily working with full stack languages like TypeScript/JavaScript in a PERN stack right now, but I&apos;m versed in Java & other technologies as well - feel free to reach out if you&apos;d like to know more!
       </div>
     </section>
   );
