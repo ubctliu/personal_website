afa532944d98d8971e5fa57369a47cb34493d8b3
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-09 11:14:49 -0800

    Change sizing again on personal title

--- src/app/components/Personal.tsx
+++ src/app/components/Personal.tsx
@@ -1,7 +1,7 @@
 const Personal = () => {
   return (
     <section>
-      <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 2xs:text-xl xs:text-2xl sm:text-3xl md:text-5xl lg:text-6xl dark:text-white"}>Hello, I&apos;m Terrence Liu!</div>
+      <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 sm:text-3xl md:text-5xl lg:text-6xl dark:text-white"}>Hello, I&apos;m Terrence Liu!</div>
       <div className={"mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"}>I&apos;m a full-stack developer from Vancouver with a passion for learning new technologies and exploring new ideas!</div>
     </section>
   );
