88b8eb29b8d94143c2b3360e71f1ab157a5aa39d
Terrence <terrence.liu@alumni.ubc.ca>
2024-02-11 21:25:20 -0800

    Changed color of intro title to normal in dark mode

--- src/app/components/Personal.tsx
+++ src/app/components/Personal.tsx
@@ -1,7 +1,7 @@
 const Personal = () => {
   return (
     <section>
-      <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 sm:text-3xl md:text-5xl lg:text-6xl dark:text-gray-300 pt-4"}>Hello, I&apos;m Terrence Liu!</div>
+      <div className={"mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 sm:text-3xl md:text-5xl lg:text-6xl dark:text-gray-900 pt-4"}>Hello, I&apos;m Terrence Liu!</div>
       <div className={"mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"}>
         I&apos;m a full-stack developer from Vancouver with a passion for learning new technologies and exploring new ideas!
         I&apos;m primarily working with full stack languages like TypeScript/JavaScript in a PERN (PostgreSQL, Express, React, Node.js) configuration right now, but I&apos;m versed in Java & other technologies as well - feel free to reach out if you&apos;d like to know more!
